7ee0341e05d972f918732927996797df31ed8281
Dany <dany@MacBook-Pro-de-Dany.local>
2024-11-27 11:16:46 +0100

    add title edit

--- todo-web/src/app/todo/todo.component.ts
+++ todo-web/src/app/todo/todo.component.ts
@@ -3,20 +3,22 @@ import { TodoService } from '../todo.service';
 import { SharedModule } from '../shared.module';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ButtonModule } from 'primeng/button';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
-  imports:[SharedModule, CheckboxModule, ButtonModule],
+  imports: [SharedModule, CheckboxModule, ButtonModule],
   standalone: true,
 })
 export class TodoComponent {
   @Input() todo!: { id: string; title: string; completed: boolean };
   @Output() deleteTask = new EventEmitter<string>();
 
-  constructor(private todoService: TodoService) {    
-  }
+  isEditing = false;
+
+  constructor(private todoService: TodoService,private toastr: ToastrService) {}
 
   onDelete() {
     this.deleteTask.emit(this.todo.id);
@@ -24,9 +26,26 @@ export class TodoComponent {
 
   onToggleCompleted() {
     this.todoService.updateTodo(this.todo.id, this.todo.completed).then(() => {
-      console.log('Tâche mise à jour:', this.todo);
+      this.toastr.success('Tâche mise à jour', "Succès")
+
     }).catch(error => {
       console.error('Erreur lors de la mise à jour de la tâche:', error);
+      this.toastr.error('Erreur lors de la mise à jour de la tâche', "Erreur")
     });
+  }
+
+  enableEdit() {
+    this.isEditing = true;
+  }
+
+  saveEdit() {
+    this.isEditing = false;
+    this.todoService.updateTodo(this.todo.id, this.todo.completed, this.todo.title)
+      .then(() => {
+        this.toastr.success('Tâche mise à jour', "Succès")
+      })
+      .catch(error => {
+        this.toastr.error('Erreur lors de la mise à jour de la tâche', "Erreur")
+      });
   }
 }
