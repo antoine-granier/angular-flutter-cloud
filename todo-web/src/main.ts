e9c28498759f2041e158d41af779dd157d3a9f87
Dany <dany@MacBook-Pro-de-Dany.local>
2024-11-29 11:38:24 +0100

    change route logic

--- todo-web/src/main.ts
+++ todo-web/src/main.ts
@@ -7,28 +7,21 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// Import environment
 import { environment } from './app/app.config';
 import { provideRouter } from '@angular/router';
-import { LoginComponent } from './app/login/login.component';
-import { HomeComponent } from './app/home/home.component';
+import { routes } from './app/app.routes';  
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
-import { authGuard } from './app/auth.guard';
 import { importProvidersFrom } from '@angular/core';
 import { provideToastr } from 'ngx-toastr';
 
-bootstrapApplication(AppComponent, {   
-  providers: [     
+bootstrapApplication(AppComponent, {
+  providers: [
     importProvidersFrom(BrowserAnimationsModule),
     provideToastr(),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),     
-    provideFirestore(() => getFirestore()),     
-    provideAuth(() => getAuth()),     
-    provideRouter([       
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomeComponent, canActivate: [authGuard] },     
-      { path: 'login', component: LoginComponent },     
-    ]),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideRouter(routes),
     { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
-  ], 
+  ],
 }).catch(err => console.error(err));
